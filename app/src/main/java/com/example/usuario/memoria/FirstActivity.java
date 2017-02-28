e5515d740a30ef947a1e11e0b79e7affd88e8f80
CaAlvarado <ca3212@gmail.com>
2017-02-28 05:26:09 -0300

    Pantalla principal
    Incluye pantalla principal mas cambios de fondo

---
+++ app/src/main/java/com/example/usuario/memoria/FirstActivity.java
@@ -0,0 +1,33 @@
+package com.example.usuario.memoria;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+        Button boton1 = (Button) findViewById(R.id.button);
+
+        boton1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        });
+
+        Button boton2 = (Button) findViewById(R.id.button2);
+
+        boton2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(getApplicationContext(), Preferences.class));
+            }
+        });
+    }
+}

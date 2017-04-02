a02851a94ed90947078fd6e88c7da0878d3e8a5a
CaAlvarado <ca3212@gmail.com>
2017-04-02 18:38:50 -0300

    Pantalla about

--- app/src/main/java/com/example/usuario/memoria/FirstActivity.java
+++ app/src/main/java/com/example/usuario/memoria/FirstActivity.java
@@ -1,7 +1,9 @@
 package com.example.usuario.memoria;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -36,6 +38,24 @@ public class FirstActivity extends AppCompatActivity {
             @Override
             public void onClick(View v){
                  finish();
+            }
+        });
+
+        Button boton4 = (Button) findViewById(R.id.button4);
+
+        boton4.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(FirstActivity.this);
+                alertDialogBuilder.setCancelable(true).setPositiveButton("Cerrar", new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface dialog, int which){
+                        dialog.cancel();
+                    }
+                });
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                alertDialog.setMessage("Esta aplicacion fue desarrollada por los alumnos Alvarado Cristian y Castillo Miguel para el CEDICA, en el marco de la materia Laboratorio de Software");
+                alertDialog.show();
             }
         });
     }
